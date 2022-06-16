e54a1a019f6979bdc84f363121651e5520c7229e
Etienne <etienne.lescot@mobile-spot.com>
2022-06-16 16:53:27 +0200

    implement getDeploymentByName

--- src/util/apis/generated/operations/codePushDeployments.js
+++ src/util/apis/generated/operations/codePushDeployments.js
@@ -739,6 +739,144 @@ function _list(ownerName, appName, options, callback) {
 }
 
 /**
+ * Get a specific deployment by its name for the given app
+ *
+ * @param {string} ownerName The name of the owner
+ *
+ * @param {string} appName The name of the application
+ *
+ * @param {object} [options] Optional Parameters.
+ *
+ * @param {object} [options.customHeaders] Headers that will be added to the
+ * request
+ *
+ * @param {function} callback - The callback.
+ *
+ * @returns {function} callback(err, result, request, response)
+ *
+ *                      {Error}  err        - The Error object if an error occurred, null otherwise.
+ *
+ *                      {array} [result]   - The deserialized result object if an error did not occur.
+ *
+ *                      {object} [request]  - The HTTP Request object if an error did not occur.
+ *
+ *                      {stream} [response] - The HTTP Response stream if an error did not occur.
+ */
+ function _getDeploymentByName(ownerName, appName, options, callback) {
+  /* jshint validthis: true */
+ let client = this.client;
+ if(!callback && typeof options === 'function') {
+   callback = options;
+   options = null;
+ }
+ if (!callback) {
+   throw new Error('callback cannot be null.');
+ }
+ // Validate
+ try {
+   if (ownerName === null || ownerName === undefined || typeof ownerName.valueOf() !== 'string') {
+     throw new Error('ownerName cannot be null or undefined and it must be of type string.');
+   }
+   if (appName === null || appName === undefined || typeof appName.valueOf() !== 'string') {
+     throw new Error('appName cannot be null or undefined and it must be of type string.');
+   }
+ } catch (error) {
+   return callback(error);
+ }
+
+ // Construct URL
+ let baseUrl = this.client.baseUri;
+ let requestUrl = baseUrl + (baseUrl.endsWith('/') ? '' : '/') + 'v0.1/apps/{owner_name}/{app_name}/deployments';
+ requestUrl = requestUrl.replace('{owner_name}', encodeURIComponent(ownerName));
+ requestUrl = requestUrl.replace('{app_name}', encodeURIComponent(appName));
+
+ // Create HTTP transport objects
+ let httpRequest = new WebResource();
+ httpRequest.method = 'GET';
+ httpRequest.url = requestUrl;
+ httpRequest.headers = {};
+ // Set Headers
+ httpRequest.headers['Content-Type'] = 'application/json; charset=utf-8';
+ if(options) {
+   for(let headerName in options['customHeaders']) {
+     if (options['customHeaders'].hasOwnProperty(headerName)) {
+       httpRequest.headers[headerName] = options['customHeaders'][headerName];
+     }
+   }
+ }
+ httpRequest.body = null;
+ // Send Request
+ return client.pipeline(httpRequest, (err, response, responseBody) => {
+   if (err) {
+     return callback(err);
+   }
+   let statusCode = response.statusCode;
+   if (statusCode !== 200) {
+     let error = new Error(responseBody);
+     error.statusCode = response.statusCode;
+     error.request = msRest.stripRequest(httpRequest);
+     error.response = msRest.stripResponse(response);
+     if (responseBody === '') responseBody = null;
+     let parsedErrorResponse;
+     try {
+       parsedErrorResponse = JSON.parse(responseBody);
+       if (parsedErrorResponse) {
+         let internalError = null;
+         if (parsedErrorResponse.error) internalError = parsedErrorResponse.error;
+         error.code = internalError ? internalError.code : parsedErrorResponse.code;
+         error.message = internalError ? internalError.message : parsedErrorResponse.message;
+       }
+       if (parsedErrorResponse !== null && parsedErrorResponse !== undefined) {
+         let resultMapper = new client.models['ListErrorModel6']().mapper();
+         error.body = client.deserialize(resultMapper, parsedErrorResponse, 'error.body');
+       }
+     } catch (defaultError) {
+       error.message = `Error "${defaultError.message}" occurred in deserializing the responseBody ` +
+                        `- "${responseBody}" for the default response.`;
+       return callback(error);
+     }
+     return callback(error);
+   }
+   // Create Result
+   let result = null;
+   if (responseBody === '') responseBody = null;
+   // Deserialize Response
+   if (statusCode === 200) {
+     let parsedResponse = null;
+     try {
+       parsedResponse = JSON.parse(responseBody);
+       result = JSON.parse(responseBody);
+       if (parsedResponse !== null && parsedResponse !== undefined) {
+         let resultMapper = {
+           required: false,
+           serializedName: 'parsedResponse',
+           type: {
+             name: 'Sequence',
+             element: {
+                 required: false,
+                 serializedName: 'ListOKResponseItemModelModelModelModelModelModelModelModelModelModelModelElementType',
+                 type: {
+                   name: 'Composite',
+                   className: 'ListOKResponseItemModelModelModelModelModelModelModelModelModelModelModel'
+                 }
+             }
+           }
+         };
+         result = client.deserialize(resultMapper, parsedResponse, 'result');
+       }
+     } catch (error) {
+       let deserializationError = new Error(`Error ${error} occurred in deserializing the responseBody - ${responseBody}`);
+       deserializationError.request = msRest.stripRequest(httpRequest);
+       deserializationError.response = msRest.stripResponse(response);
+       return callback(deserializationError);
+     }
+   }
+
+   return callback(null, result, httpRequest, response);
+ });
+}
+
+/**
  * Creates a CodePush Deployment for the given app
  *
  * @param {string} ownerName The name of the owner
@@ -945,6 +1083,7 @@ class CodePushDeployments {
     this._get = _get;
     this._update = _update;
     this._list = _list;
+    this._getDeploymentByName = _getDeploymentByName;
     this._create = _create;
   }
 
@@ -1434,6 +1573,59 @@ class CodePushDeployments {
       });
     } else {
       return self._list(ownerName, appName, options, optionalCallback);
+    }
+  }
+
+  /**
+   * Gets a specific deployment by its name for the given app
+   *
+   * @param {string} ownerName The name of the owner
+   *
+   * @param {string} appName The name of the application
+   *
+   * @param {object} [options] Optional Parameters.
+   *
+   * @param {object} [options.customHeaders] Headers that will be added to the
+   * request
+   *
+   * @param {function} [optionalCallback] - The optional callback.
+   *
+   * @returns {function|Promise} If a callback was passed as the last parameter
+   * then it returns the callback else returns a Promise.
+   *
+   * {Promise} A promise is returned
+   *
+   *                      @resolve {Array} - The deserialized result object.
+   *
+   *                      @reject {Error} - The error object.
+   *
+   * {function} optionalCallback(err, result, request, response)
+   *
+   *                      {Error}  err        - The Error object if an error occurred, null otherwise.
+   *
+   *                      {array} [result]   - The deserialized result object if an error did not occur.
+   *
+   *                      {object} [request]  - The HTTP Request object if an error did not occur.
+   *
+   *                      {stream} [response] - The HTTP Response stream if an error did not occur.
+   */
+   getDeploymentByName(ownerName, appName, options, optionalCallback) {
+    let client = this.client;
+    let self = this;
+    if (!optionalCallback && typeof options === 'function') {
+      optionalCallback = options;
+      options = null;
+    }
+    if (!optionalCallback) {
+      return new Promise((resolve, reject) => {
+        self._getDeploymentByName(ownerName, appName, options, (err, result, request, response) => {
+          if (err) { reject(err); }
+          else { resolve(result); }
+          return;
+        });
+      });
+    } else {
+      return self._getDeploymentByName(ownerName, appName, options, optionalCallback);
     }
   }
 
